17d24a2b10f34c1b447ad39ce14f610906a55a64
NavyHubb <sngjae972@gmail.com>
2023-04-05 21:18:40 +0900

    init

---
+++ src/main/java/com/green/tablecheck/TableCheckApplication.java
@@ -0,0 +1,13 @@
+package com.green.tablecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/tablecheck/TableCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.tablecheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
