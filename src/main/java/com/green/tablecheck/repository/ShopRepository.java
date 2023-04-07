6ceae687da17149c9bf8ca2989df267df18d9f80
NavyHubb <sngjae972@gmail.com>
2023-04-07 19:38:47 +0900

    feat(상점 상세정보 조회 기능 구현):

--- src/main/java/com/green/tablecheck/repository/ShopRepository.java
+++ src/main/java/com/green/tablecheck/repository/ShopRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShopRepository extends JpaRepository<Shop, Long> {
+
     Optional<Shop> findByManagerId(Long managerId);
 
     Optional<Shop> findByName(String name);
+
 }
