ae22101829a505ae0fdd1c534af006979f8f4f8a
NavyHubb <sngjae972@gmail.com>
2023-04-10 20:41:11 +0900

    feat: 상점 상세정보 조회 기능 구현
    추가 변동 사항: DTO에 대한 Validation

---
+++ src/main/java/com/green/tablecheck/domain/dto/ReservationDto.java
@@ -0,0 +1,30 @@
+package com.green.tablecheck.domain.dto;
+
+import com.green.tablecheck.domain.model.Reservation;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReservationDto {
+
+    private String shopName;
+    private String customerName;
+    private LocalDateTime dateTime;
+
+    public static ReservationDto from(Reservation reservation) {
+        return ReservationDto.builder()
+            .shopName(reservation.getShop().getName())
+            .customerName(reservation.getCustomer().getName())
+            .dateTime(reservation.getDateTime())
+            .build();
+    }
+
+}
---
+++ src/main/java/com/green/tablecheck/domain/model/Reservation.java
@@ -0,0 +1,42 @@
+package com.green.tablecheck.domain.model;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reservation_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shop_id")
+    private Shop shop;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    private LocalDateTime dateTime;
+    private int peopleCount;
+
+}
